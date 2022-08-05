497f342daa2348d3101317d593d0932bd22725e3
NickYOOO <outgrowthr@gmail.com>
2022-08-05 20:45:09 +0900

    변경사항 :

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import styled from "styled-components"
-
+import WriteBoard from "./view/WriteBoard"
 
 function App() {
   return (
     <Container>
+      <WriteBoard />
     </Container>
   );
 }
