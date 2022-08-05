4d91dcb8fde51621ad2a8cf06397ba99ce706ea2
NickYOOO <outgrowthr@gmail.com>
2022-08-05 20:45:09 +0900

    변경사항 :

--- src/view/WriteBoard.jsx
+++ src/view/WriteBoard.jsx
@@ -1,34 +1,42 @@
-import React from "react";
-import "./Create.css";
-import { useNavigate, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-// import Header from "";
-// import Buttons from "";
-// import Layout from "";
-// import Board from "";
+import React from "react"; //가져오자~
+import styled from "styled-components"; // styled-components에서 styled 키워드 가져와
+import Header from "../components/header/Header"; //components Header 연결해줌
 
-const Create = () => {
-  const navigate = useNavigate();
-  const container = useSelector((state) => state.todo.list);
-
+function WriteBoard() {
   return (
-    <div className="body">
-      <div className="box-wrapper">
-        <div className="box">
-          <h1> 작성자 </h1>
-          <p> 내용 </p>
-          <div>
-            <button className="back-button" onClick={() => navigate("/")}>
-              등록
-            </button>
-            <button className="back-button" onClick={() => navigate("/")}>
-              취소
-            </button>
-          </div>
+    <A>
+      <B>
+        <p> 작성자 </p>
+        <p> 내용 </p>
+        <div>
+          <button style={{ margin: "0 10px" }}>등록</button>
+          <button>취소</button>
         </div>
-      </div>
-    </div>
+      </B>
+    </A>
   );
-};
+}
 
-export default Create;
+const A = styled.div`
+  font-size: 30px;
+  border: 3px solid rgb(176, 80, 80);
+  border-radius: 12px;
+  padding: 12px 24px 24px;
+  width: 600px;
+  height: 700px;
+  display: flex;
+ 
+`;
+const B = styled.div`
+  .body {
+    border: 2px solid rgb(238, 238, 238);
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+  }
+`;
+export default WriteBoard; //내보내자~
