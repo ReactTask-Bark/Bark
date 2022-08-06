6c954f13408436288e737c02c67c26fc428140b7
NickYOOO <outgrowthr@gmail.com>
2022-08-07 00:01:03 +0900

    submain 참조하여 header, button 따라만듬

--- src/view/WriteBoard.jsx
+++ src/view/WriteBoard.jsx
@@ -1,58 +1,102 @@
 import React from "react"; //가져오자~
 import styled from "styled-components"; // styled-components에서 styled 키워드 가져와
-import Header from "../components/header/Header"; //components Header 연결해줌
+import Header from "components/header/Header";
 
 function WriteBoard() {
   return (
-    <Board>
-      <HeadArea>
+    <>
+      <Layout>
+        <Header />
+      </Layout>
+      <Board>
+        <HeadArea>
+          <div>
+            <div>제목 : </div>
+            <div>내용 : </div>
+          </div>
+
+          <div>내가 제목이다!</div>
+        </HeadArea>
+
         <div>
-          <div>제목 : </div>
-          <div>내용 : </div>
+          <Contents>내가 내용이다!</Contents>
+          <ButtonArea>
+            <Btn>수정</Btn>
+            <Btn>삭제</Btn>
+            <Btn>뒤로가기</Btn>
+          </ButtonArea>
         </div>
-
-        <div>내가 제목이다!</div>
-      </HeadArea>
-
-      <div>
-        <Contents>내가 내용이다!</Contents>
-        <ButtonArea>
-          <button>수정</button>
-          <button>삭제</button>
-          <button>뒤로가기</button>
-        </ButtonArea>
-      </div>
-    </Board>
+      </Board>
+    </>
   );
 }
 
+const Layout = styled.div`
+  max-width: 1200px;
+  min-width: 800px;
+  margin: auto;
+`;
+
+
 const Contents = styled.div`
   height: 300px;
-  border: 1px solid red;
+  margin: 10px;
+  border: 1px solid rgb(226, 128, 128);
   border-radius: 15px;
   box-shadow: 0 0 5px 0;
+
+  display: flex;
+  font-size: 15px;
+  font-weight: bold;
+  color: #e2a9a9;
+  
 `;
 
 const Board = styled.div`
   width: 600px;
-  height: 600px;
-  margin: 30px auto;
-  border: 1px solid black;
+  height: 550px;
+  margin: 10px auto;
+  border: 1px solid rgb(255, 204, 204);
   border-radius: 15px;
   box-shadow: 0 0 5px 0;
-
 `;
 
 const HeadArea = styled.div`
   display: flex;
-  justify-content: space-between;;
+  justify-content: space-between;
   margin: 10px;
-  
+
+  display: flex;
+  font-size: 15px;
+  font-weight: bold;
+  color: #e2a9a9;
 `;
 
 const ButtonArea = styled.div`
   display: flex;
   justify-content: center;
   align-items: flex-start;
+`;
+const Btn = styled.button`
+  cursor: pointer;
+  width: 45px;
+  height: 45px;
+
+  background: rgb(255, 204, 204);
+  border: 2px solid rgb(255, 204, 204);
+  border-radius: 50%;
+
+  align-items:center;
+  text-align: center;
+  display: flex;
+  font-size: 15px;
+  font-weight: bold;
+  color: white;
+  
+  transition-duration: 0.5s;
+  &:hover {
+    background-color: white;
+    color: rgb(255, 204, 204);
+  }
 `;
 export default WriteBoard;
