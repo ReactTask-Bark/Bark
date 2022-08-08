8bc6d56b3f76af5a83f0ad721b6220d8a29c5ebb
yeun38 <choh1997@naver.com>
2022-08-08 10:36:06 +0900

    Router setting

--- src/components/form/FixedForm.jsx
+++ src/components/form/FixedForm.jsx
@@ -1,35 +1,42 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const FixedForm = () => {
-    return(
-    <Form>
-        <AddBtn>+</AddBtn>
+  const navigate = useNavigate();
+  return (
+    <Form
+      onClick={() => {
+        navigate("/writepost");
+      }}
+    >
+      <AddBtn>+</AddBtn>
     </Form>
-    )
-}
+  );
+};
 
 const Form = styled.div`
-    position: fixed;
-    bottom: 100px; right: 25vw;
-`
+  position: fixed;
+  bottom: 100px;
+  right: 25vw;
+`;
 const AddBtn = styled.button`
-    cursor: pointer;
-    width: 80px;
-    height: 80px;
-    
-    background: rgb(255,204,204);
-    border: 2px solid rgb(255,204,204);
-    border-radius: 50%;
-    
-    font-size: 50px;
-    font-weight: bold;
-    color: white;
+  cursor: pointer;
+  width: 80px;
+  height: 80px;
 
-    transition-duration: .5s;
-    &:hover {
-        background-color: white;
-        color: rgb(255,204,204);
-    }
-`
+  background: rgb(255, 204, 204);
+  border: 2px solid rgb(255, 204, 204);
+  border-radius: 50%;
 
-export default FixedForm
+  font-size: 50px;
+  font-weight: bold;
+  color: white;
+
+  transition-duration: 0.5s;
+  &:hover {
+    background-color: white;
+    color: rgb(255, 204, 204);
+  }
+`;
+
+export default FixedForm;
