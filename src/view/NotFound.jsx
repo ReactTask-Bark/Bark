6c0bd50729bd1d0cd983ca03694c0e7f55bdd5a7
somfist <qkrruddjs317@naver.com>
2022-08-08 22:45:57 +0900

    NotFound 대략적으로 만듬 1. 2초뒤 홈으로 가기 2. 화면누르면 홈으로 가기

--- src/view/NotFound.jsx
+++ src/view/NotFound.jsx
@@ -1,5 +1,22 @@
-const NotFound = () => {
-  return <div>페이지없습니다.</div>;
-};
+import styled, { keyframes } from "styled-components"
 
-export default NotFound;
+import { useNavigate } from "react-router-dom"
+
+const NotFound = () => {
+    const navigate = useNavigate("")
+    const goHome = () => navigate("/")
+    setTimeout(goHome, 2000)
+    return (
+        <Page className="fcc" onClick={goHome}>
+            <h2 style={{color: "red"}}>없는 페이지 입니다.</h2>
+        </Page>
+    )
+}
+
+const Page = styled.div`
+    position: fixed;
+    top:0; right: 0; bottom: 0; left: 0;
+    background: rgb(255,204,204);
+`
+
+export default NotFound
