c9897445cd76cf4b3ac8b273177899fdf36a513e
yeun38 <choh1997@naver.com>
2022-08-10 13:10:14 +0900

    read detail

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -1,44 +1,48 @@
-import { createAsyncThunk ,createSlice } from "@reduxjs/toolkit";
-import axios from "apis/axiosInstance"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "apis/axiosInstance";
 
-const initialState = {          // 초기 값
-    isLoading: false,
-    list : []
-}
+const initialState = {
+  // 초기 값
+  isLoading: false,
+  list: [],
+};
 
 export const _LoadPostAll = createAsyncThunk(
-	// 첫번째 인자 : action value
-  "_LoadPostAll", 
-	// 두번째 인자 : 콜백함수 
+  // 첫번째 인자 : action value
+  "_LoadPostAll",
+  // 두번째 인자 : 콜백함수
   async (payload, api) => {
     try {
-        const postList = await axios.get(process.env.REACT_APP_POSTPATH)
-        const sliceList = postList.data.slice(0, payload + 5)
-        return api.fulfillWithValue(sliceList)
-    }catch(err) {
-        return api.rejectWithValue(err)
+      const postList = await axios.get(
+        process.env.REACT_APP_POSTPATH + "/post"
+      );
+      const sliceList = postList.data.slice(0, payload + 5);
+      return api.fulfillWithValue(sliceList);
+    } catch (err) {
+      return api.rejectWithValue(err);
     }
   }
 );
 
 const postSlice = createSlice({
-    name: "postSlice",          // 이 모듈의 이름
-    initialState,               // 이 모듈의 초기상태 값
-    reducers: {                 // 이 모듈의 Reducer 로직이자, 액션크리에이터
+  name: "postSlice", // 이 모듈의 이름
+  initialState, // 이 모듈의 초기상태 값
+  reducers: {
+    // 이 모듈의 Reducer 로직이자, 액션크리에이터
+  },
+  extraReducers: {
+    [_LoadPostAll.pending]: (state) => {
+      state.isLoading = true;
     },
-    extraReducers: {
-        [_LoadPostAll.pending]: (state) => {
-            state.isLoading = true
-        },
-        [_LoadPostAll.fulfilled]: (state, action) => {
-            state.isLoading = false
-            state.list = action.payload
-        },
-        [_LoadPostAll.rejected]: (state, action) => {
-            state.isLoading = false;
-            console.log(action.payload)
-        }
+    [_LoadPostAll.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.list = action.payload;
     },
+    [_LoadPostAll.rejected]: (state, action) => {
+      state.isLoading = false;
+      console.log(action.payload);
+    },
+  },
 });
 
 // action생성자 내보내기 => 쓰실때 주석 풀기
